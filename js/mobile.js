bef4911832b7a71d1e1363c92810184e7bc66c9d
Azt1 <inho.ilha@hotmail.com>
2022-07-05 17:44:31 -0300

    commit pt4

--- js/mobile.js
+++ js/mobile.js
@@ -1,4 +1,49 @@
-const form = document.querySelector('#formulário');
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
+    }
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+  );
+  mobileNavbar.init();
+
+  const form = document.querySelector('#formulário');
 
 form.addEventListener('submit', function (e) {
     e.preventDefault ();
@@ -57,7 +102,7 @@ function getImc (peso, altura) {
 
 }
 
-function criaP(className) {
+function criaP() {
     const p = document.createElement ('p');
     p.classList.add('className');
     return p;
